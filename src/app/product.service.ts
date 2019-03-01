49391e27b08c6d178dd0c78a932ef07e71277614
prabhanjan <prabhanjan.t.r@gmail.com>
2019-03-01 12:37:39 +0530

    bug fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,12 +9,12 @@ import{Product} from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _prductUrl = '../assets/products.json'
+  private _producstUrl = '../assets/products.json'
   constructor(private _http:Http) { }
 getAlbum(id:number):Observable<Album>{
   return this._http.get(this._albumUrl).map((response)=><Album>response.json())
 }
 getProducts():Observable<Product[]>{
-  return this._http.get(this._prductUrl).map((response)=><Product[]>response.json());
+  return this._http.get(this._producstUrl).map((response)=><Product[]>response.json());
 }
 }
